d404731be9bda7e63589b698708a6376fcf2dfbd
Max <maxim.macari.dam@gmail.com>
2020-10-19 15:42:08 +0200

    Initial Commit

---
+++ LoginPageModifieres/LoginPageModifieresApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginPageModifieresApp.swift
+//  LoginPageModifieres
+//
+//  Created by Maxim Macari on 19/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct LoginPageModifieresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
